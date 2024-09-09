8d8837aa9d767aab830882348d761216fcd8a909
Chonnawee aeaumsang <chonnawee_aeaumsang@cmu.ac.th>
2024-09-09 14:10:48 +0700

    Update#4

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const TelegramBot = require("node-telegram-bot-api");
 const TOKEN = "7498251188:AAGkKHnIgMpfAwQwr3Ygu0h6uzAcHyK89-8"; // Replace with your bot token
 const webhookUrl = "https://i-ai-swipe.vercel.app/api/webhook"; // This should match your deployed function URL
 
-const bot = new TelegramBot(TOKEN);
+const bot = new TelegramBot(TOKEN, { polling: false });
 
 bot.setWebHook(webhookUrl).then(() => {
     console.log("Webhook set successfully.");
